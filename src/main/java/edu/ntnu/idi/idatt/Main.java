4a7d3bf232a7299f28fadb64d4d82cbe0b169794
Shakti Om Sharma <shaktiforgithub@gmail.com>
2024-12-11 16:48:15 +0100

    lagt til flere plantUML diagrammer

--- src/main/java/edu/ntnu/idi/idatt/Main.java
+++ src/main/java/edu/ntnu/idi/idatt/Main.java
@@ -6,20 +6,15 @@ import edu.ntnu.idi.idatt.managers.Kitchenmanager;
  * Wrapper class for the static main method to run the application.
  */
 public class Main {
-      /**
+
+    /**
      * Main method - entry point of the application.
-     *
-     * 
      */
     public static void main(String[] args) {
-
         Kitchenmanager kitchenManager = new Kitchenmanager();
 
-         // Initialiserer programmet med standarddata
+        // Initialiserer programmet med standarddata
         kitchenManager.init();
-
         kitchenManager.start();
-  }
+    }
 }
-
-
